4ea6ac75bce8bbf462a28f49570b64c7b0aea545
jother043 <josemgh166@gmail.com>
2023-04-10 08:00:54 +0200

    Segundo commit del repositorio

--- src/Main.java
+++ src/Main.java
@@ -33,22 +33,20 @@ public class Main {
                         System.out.println("Opción inválida");
                 }
             } while (opciones > 0 && opciones < 6);
-        } catch (Exception e) {
-            System.out.println("Error: " + e.getMessage());
+
         }finally {
             sc.close();
         }
     }
 
     public static String menu() {
-        StringBuilder sb = new StringBuilder();
-        sb.append("1. Listar contenido directorio").append(System.lineSeparator());
-        sb.append("2. Listar directorio y buscar fichero que comiencen con una palabra").append(System.lineSeparator());
-        sb.append("3. Listar archivos con cierta extension en un directorio").append(System.lineSeparator());
-        sb.append("4. Buscar un archivo en un directorio").append(System.lineSeparator());
-        sb.append("5. Buscar recursivamente un archivo en un directorio").append(System.lineSeparator());
-        sb.append("6. Salir").append(System.lineSeparator());
-        return sb.toString();
+        String sb = "1. Listar contenido directorio" + System.lineSeparator() +
+                "2. Listar directorio y buscar fichero que comiencen con una palabra" + System.lineSeparator() +
+                "3. Listar archivos con cierta extension en un directorio" + System.lineSeparator() +
+                "4. Buscar un archivo en un directorio" + System.lineSeparator() +
+                "5. Buscar recursivamente un archivo en un directorio" + System.lineSeparator() +
+                "6. Salir" + System.lineSeparator();
+        return sb;
     }
 
     /**
@@ -166,6 +164,59 @@ public class Main {
 
     public static void buscarArchivoRecursivo() {
 
+        int repetidos = 0;
+        boolean encontrado = false;
+
+        System.out.println("Introduce el nombre del directorio: ");
+        File directorio = new File(sc.next());
+        //Si el directorio pasado es un directorio entonces pasamos el contenido al array.
+        if (directorio.isDirectory()) {
+            //Creamos un array de tipo File para almacenar el contenido del directorio.
+            File[] contenido = directorio.listFiles();
+
+            System.out.println("Introduce el nombre del archivo a buscar: ");
+            String nombre = sc.next();
+            //Si el array no es nulo entonces recorremos el array y mostramos el contenido.
+            if (contenido != null) {
+                //Recorremos el array.
+                for (File fichero : contenido) {
+                    /*
+                     * Si el elemento comienza con la palabra introducida y es un fichero entonces lo mostramos.
+                     * El método endsWith() comprueba si el String termina con la palabra introducida.
+                     * El método isFile() comprueba si el elemento es un fichero.
+                     */
+                    if (fichero.getName().equals(nombre) && fichero.isFile()) {
+                        //Si el elemento es un fichero entonces lo mostramos como fichero.
+                        repetidos++;
+                        System.out.println("Fichero: " + repetidos + " " + fichero.getName());
+                        encontrado = true;
+                    }
+
+                    if (fichero.isDirectory() && fichero.listFiles() != null) {
+                        busquedaRecursiva(fichero, nombre);
+                    }
+                }
+                if(encontrado == false){
+                    System.out.println("No se ha encontrado el archivo");
+                }
+            }
+
+        }else{
+            System.err.println("El directorio no existe");
+        }
+
     }
 
+    public static void busquedaRecursiva(File directorio, String nombre) {
+        int repetidos = 2;
+        File[] contenido = directorio.listFiles();
+        if (contenido != null) {
+            for (File fichero : contenido) {
+                if (fichero.getName().equals(nombre) && fichero.isFile()) {
+                    System.out.println("Fichero: "+ repetidos + " " + fichero.getName());
+                }
+
+            }
+        }
+    }
 }
